17fb95bd9f0dc78971a49b1be663614d29b95fab
dengnh <dengninghai@shinemo.com>
2022-11-11 18:08:04 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <child ref="childRef" />
+</template>
+
+<script lang="ts">
+import { ref, onMounted } from "vue";
+import child from "./components/child/index.vue";
+
+export default {
+  components: { child },
+  setup() {
+    let childRef = ref(null);
+    onMounted(() => {
+      console.log(child);
+      console.log(child.value.name);
+      console.log(child.value.age);
+    });
+
+    return { childRef };
+  },
+};
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/child/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>子组件</div>
+</template>
+
+<script>
+import { ref } from "vue";
+export default {
+  setup() {
+    let name = ref("dnhyxc");
+    let age = ref(18);
+
+    return { name, age };
+  },
+};
+</script>
